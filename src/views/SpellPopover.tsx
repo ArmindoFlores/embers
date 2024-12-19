2735f43c49b3e3732172dd416aebab65bdc09787
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2024-12-19 18:49:01 +0000

    Add a most recent list of selected effects

--- src/views/SpellPopover.tsx
+++ src/views/SpellPopover.tsx
@@ -5,17 +5,32 @@ import { toolID, toolMetadataSelectedSpell } from "../targetTool";
 import { useEffect, useState } from "react";
 
 import { APP_KEY } from "../config";
-import { Effect } from "../types/effects";
 import OBR from "@owlbear-rodeo/sdk";
 import { useOBR } from "../react-obr/providers";
 
+const MAX_RECENT_EFFECTS = 5;
 export const spellPopoverId = `${APP_KEY}/spell-popover`;
+export const mostRecentEffectsMetadataKey = `${APP_KEY}/most-recent-effects`;
 
-function selectEffect(effectName: string) {
-    OBR.tool.setMetadata(
+async function selectEffect(effectName: string) {
+    // Update recent spells list
+    const mostRecentEffectsList = (await getMostRecentEffects()).filter(
+        name => name != effectName
+    );
+    mostRecentEffectsList.splice(0, 0, effectName);
+    if (mostRecentEffectsList.length > MAX_RECENT_EFFECTS) {
+        mostRecentEffectsList.pop();
+    }
+    await OBR.player.setMetadata({ [mostRecentEffectsMetadataKey]: mostRecentEffectsList });
+
+    // Set selected effect
+    await OBR.tool.setMetadata(
         toolID, 
         { [toolMetadataSelectedSpell]: effectName }
-    ).then(() => OBR.popover.close(spellPopoverId));
+    );
+
+    // Close this popover
+    await OBR.popover.close(spellPopoverId);
 }
 
 function checkForEscape(event: React.KeyboardEvent<HTMLDivElement>) {
@@ -24,27 +39,39 @@ function checkForEscape(event: React.KeyboardEvent<HTMLDivElement>) {
     }
 }
 
+async function getMostRecentEffects() {
+    const metadata = await OBR.player.getMetadata();
+    if (metadata == undefined) {
+        return [];
+    }
+    const mostRecentEffectsList = metadata[mostRecentEffectsMetadataKey];
+    if (!Array.isArray(mostRecentEffectsList) || mostRecentEffectsList.length == 0) {
+        return [];
+    }
+    return mostRecentEffectsList as string[];
+}
+
+async function getSortedEffectList() {
+    const mostRecentEffectsList = await getMostRecentEffects();
+    const effectNamesWithoutMostRecent = effectNames.filter(name => !mostRecentEffectsList.includes(name));
+    return mostRecentEffectsList.concat(effectNamesWithoutMostRecent);
+}
+
+function normalizeSearch(str: string) {
+    return str.toLowerCase().replace("_", " ").replace(".", " ");
+}
+
 export default function SpellPopover() {
     const obr = useOBR();
     const [search, setSearch] = useState("");
-    const [selectedName, setSelectedName] = useState<string>();
-    const [selected, setSelected] = useState<Effect>();
+    const [sortedEffectsList, setSortedEffectsList] = useState<string[]>([]);
 
     useEffect(() => {
         if (!obr.ready || !obr.sceneReady) {
             return;
         }
 
-        OBR.tool.getMetadata(toolID).then(metadata => {
-            const selected = metadata?.[toolMetadataSelectedSpell] as string|undefined;
-            if (selected != undefined) {
-                const effect = getEffect(selected);
-                if (effect != undefined) {
-                    setSelected(effect);
-                    setSelectedName(selected);
-                }
-            }
-        });
+        getSortedEffectList().then(list => setSortedEffectsList(list));
     }, [obr.ready, obr.sceneReady]);
 
     if (!obr.ready) {
@@ -56,14 +83,14 @@ export default function SpellPopover() {
                 <input type="text" className="search-input" placeholder="Type to search..." autoFocus value={search} onChange={event => setSearch(event.target.value)} />
                 <ul className="results-list">
                     {
-                        selected && selectedName &&
-                        <li onClick={() => selectEffect(selectedName)} className="selected">
-                            <img src={`${window.location.origin}/Library/${selected.thumbnail}`} loading="lazy" />
-                            <p className="spell-name">{ selectedName }</p>
+                        sortedEffectsList.length > 0 &&
+                        <li onClick={() => selectEffect(sortedEffectsList[0])} className="selected">
+                            <img src={`${window.location.origin}/Library/${getEffect(sortedEffectsList[0])!.thumbnail}`} loading="lazy" />
+                            <p className="spell-name">{ sortedEffectsList[0] }</p>
                         </li>
                     }
                     {
-                        effectNames.filter(effectName => effectName != selectedName && effectName.toLowerCase().includes(search.toLowerCase())).map(effectName => {
+                        sortedEffectsList.slice(1).filter(name => normalizeSearch(name).includes(normalizeSearch(search))).map(effectName => {
                             const effect = getEffect(effectName);
                             return <li key={effectName} onClick={() => selectEffect(effectName)}>
                                 <img src={`${window.location.origin}/Library/${effect!.thumbnail}`} loading="lazy" />
