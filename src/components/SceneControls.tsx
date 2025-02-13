39383ab56983a0c8465a73878fd7301ebf0d783a
Aji Banawan <jimuel.banawan@ibm.com>
2025-02-14 02:00:54 +0800

    add in missed header component

--- src/components/SceneControls.tsx
+++ src/components/SceneControls.tsx
@@ -1,6 +1,13 @@
 import "./SceneControls.css";
 
-import { FaArrowPointer, FaEye, FaEyeSlash, FaLink, FaLinkSlash, FaSquareMinus } from "react-icons/fa6";
+import {
+    FaArrowPointer,
+    FaEye,
+    FaEyeSlash,
+    FaLink,
+    FaLinkSlash,
+    FaSquareMinus,
+} from "react-icons/fa6";
 import OBR, { Item, Player } from "@owlbear-rodeo/sdk";
 import { destroySpell, getSpell } from "../effects/spells";
 import { effectMetadataKey, spellMetadataKey } from "../effects/effects";
@@ -9,8 +16,19 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 import { MessageType } from "../types/messageListener";
 import { Spell } from "../types/spells";
 import { useOBR } from "../react-obr/providers";
+import { Typography } from "@mui/material";
 
-function SpellDisplay({ spellID, effectID, item, caster }: { spellID?: string, effectID?: string, item: Item, caster: Player }) {
+function SpellDisplay({
+    spellID,
+    effectID,
+    item,
+    caster,
+}: {
+    spellID?: string;
+    effectID?: string;
+    item: Item;
+    caster: Player;
+}) {
     const obr = useOBR();
     const [spell, setSpell] = useState<Spell>();
     const [attachedToName, setAttachedToName] = useState<string>();
@@ -21,7 +39,7 @@ function SpellDisplay({ spellID, effectID, item, caster }: { spellID?: string, e
     }, [item]);
 
     const toggleItemVisibility = useCallback(() => {
-        OBR.scene.items.updateItems([item], items => {
+        OBR.scene.items.updateItems([item], (items) => {
             for (const itemDraft of items) {
                 itemDraft.visible = !item.visible;
             }
@@ -29,7 +47,7 @@ function SpellDisplay({ spellID, effectID, item, caster }: { spellID?: string, e
     }, [item]);
 
     const toggleItemDisableHit = useCallback(() => {
-        OBR.scene.items.updateItems([item], items => {
+        OBR.scene.items.updateItems([item], (items) => {
             for (const itemDraft of items) {
                 itemDraft.disableHit = !item.disableHit;
             }
@@ -37,7 +55,11 @@ function SpellDisplay({ spellID, effectID, item, caster }: { spellID?: string, e
     }, [item]);
 
     const deleteItem = useCallback(() => {
-        if (spellID != undefined && spell?.onDestroyBlueprints && spell.onDestroyBlueprints.length > 0) {
+        if (
+            spellID != undefined &&
+            spell?.onDestroyBlueprints &&
+            spell.onDestroyBlueprints.length > 0
+        ) {
             destroySpell(spellID, caster.id, [item]);
         }
         OBR.scene.items.deleteItems([item.id]);
@@ -49,8 +71,7 @@ function SpellDisplay({ spellID, effectID, item, caster }: { spellID?: string, e
         }
         if (obr.player.role != "GM" && isGM) {
             setIsGM(false);
-        }
-        else if (obr.player.role == "GM" && !isGM) {
+        } else if (obr.player.role == "GM" && !isGM) {
             setIsGM(true);
         }
     }, [obr.ready, obr.player?.role, isGM]);
@@ -64,7 +85,9 @@ function SpellDisplay({ spellID, effectID, item, caster }: { spellID?: string, e
 
     useEffect(() => {
         if (item.attachedTo != undefined) {
-            OBR.scene.items.getItems([item.attachedTo]).then(item => setAttachedToName(item[0]?.name));
+            OBR.scene.items
+                .getItems([item.attachedTo])
+                .then((item) => setAttachedToName(item[0]?.name));
         }
     }, [item.attachedTo]);
 
@@ -72,39 +95,66 @@ function SpellDisplay({ spellID, effectID, item, caster }: { spellID?: string, e
         return null;
     }
 
-    return <div className="scene-spell-display-item">
-        <p title={`Spell name: ${spell.name}\nEffect ID: ${effectID}\nAttached to: ${attachedToName ?? "nothing"}\nCaster: ${caster.name}`}> { spell.name }</p>
-        <div className="scene-spell-display-controls">
-            <div className="scene-spell-display-control-button" onClick={selectItem} title="Select this effect">
-                <FaArrowPointer />
-            </div>
-            <div className="scene-spell-display-control-button" onClick={toggleItemDisableHit} title={item.disableHit ? "Enable hit" : "Disable hit"}>
-                {
-                    item.disableHit ? <FaLinkSlash /> : <FaLink />
-                }
-            </div>
-            <div className="scene-spell-display-control-button" onClick={toggleItemVisibility} title={item.visible ? "Hide effect" : "Show effect"}>
-                {
-                    item.visible ? <FaEye /> : <FaEyeSlash />
-                }
-            </div>
-            <div className="scene-spell-display-control-button" onClick={deleteItem} title="Delete this effect">
-                <FaSquareMinus />
+    return (
+        <div className="scene-spell-display-item">
+            <p
+                title={`Spell name: ${
+                    spell.name
+                }\nEffect ID: ${effectID}\nAttached to: ${
+                    attachedToName ?? "nothing"
+                }\nCaster: ${caster.name}`}
+            >
+                {" "}
+                {spell.name}
+            </p>
+            <div className="scene-spell-display-controls">
+                <div
+                    className="scene-spell-display-control-button"
+                    onClick={selectItem}
+                    title="Select this effect"
+                >
+                    <FaArrowPointer />
+                </div>
+                <div
+                    className="scene-spell-display-control-button"
+                    onClick={toggleItemDisableHit}
+                    title={item.disableHit ? "Enable hit" : "Disable hit"}
+                >
+                    {item.disableHit ? <FaLinkSlash /> : <FaLink />}
+                </div>
+                <div
+                    className="scene-spell-display-control-button"
+                    onClick={toggleItemVisibility}
+                    title={item.visible ? "Hide effect" : "Show effect"}
+                >
+                    {item.visible ? <FaEye /> : <FaEyeSlash />}
+                </div>
+                <div
+                    className="scene-spell-display-control-button"
+                    onClick={deleteItem}
+                    title="Delete this effect"
+                >
+                    <FaSquareMinus />
+                </div>
             </div>
         </div>
-    </div>;
+    );
 }
 
 export default function SceneControls() {
     const obr = useOBR();
     const [party, setParty] = useState<Player[]>([]);
-    const [player, setPlayer] = useState<Player|null>(null);
+    const [player, setPlayer] = useState<Player | null>(null);
     const [globalSpellItems, _setGlobalSpellItems] = useState<Item[]>([]);
 
     const spellEffectsPresent = useMemo(() => {
-        const playerIDs = [player, ...party].map(player => player?.id).filter(player => player != undefined);
+        const playerIDs = [player, ...party]
+            .map((player) => player?.id)
+            .filter((player) => player != undefined);
         for (const item of globalSpellItems) {
-            const caster = (item.metadata[spellMetadataKey] as MessageType["spellData"])?.caster;
+            const caster = (
+                item.metadata[spellMetadataKey] as MessageType["spellData"]
+            )?.caster;
             if (caster != undefined && playerIDs.includes(caster)) {
                 return true;
             }
@@ -112,41 +162,65 @@ export default function SceneControls() {
         return false;
     }, [player, party, globalSpellItems]);
 
-    const setGlobalSpellItems = useCallback((items: Item[]) => (
-        _setGlobalSpellItems(items.filter(item => (
-            item.metadata[effectMetadataKey] != undefined || item.metadata[spellMetadataKey] != undefined
-        )))
-    ), []);
+    const setGlobalSpellItems = useCallback(
+        (items: Item[]) =>
+            _setGlobalSpellItems(
+                items.filter(
+                    (item) =>
+                        item.metadata[effectMetadataKey] != undefined ||
+                        item.metadata[spellMetadataKey] != undefined
+                )
+            ),
+        []
+    );
 
-    const PlayerEffects = useCallback(({ player }: { player: Player }) => {
-        const playerItems = globalSpellItems.filter(
-            item => (
-                (item.metadata[spellMetadataKey] as MessageType["spellData"])?.caster === player.id &&
-                (item.metadata[spellMetadataKey] as MessageType["spellData"])?.name != undefined
-            )
-        );
+    const PlayerEffects = useCallback(
+        ({ player }: { player: Player }) => {
+            const playerItems = globalSpellItems.filter(
+                (item) =>
+                    (
+                        item.metadata[
+                            spellMetadataKey
+                        ] as MessageType["spellData"]
+                    )?.caster === player.id &&
+                    (
+                        item.metadata[
+                            spellMetadataKey
+                        ] as MessageType["spellData"]
+                    )?.name != undefined
+            );
 
-        if (playerItems.length === 0) {
-            return null;
-        }
+            if (playerItems.length === 0) {
+                return null;
+            }
 
-        return <div>
-            <p className="bold">{ player.name }</p>
-            <ul className="scene-spell-list">
-                {
-                    playerItems.map(item => (
-                        <SpellDisplay
-                            key={item.id}
-                            spellID={(item.metadata[spellMetadataKey] as MessageType["spellData"])?.name}
-                            effectID={item.metadata[effectMetadataKey] as string}
-                            item={item}
-                            caster={player}
-                        />
-                    ))
-                }
-            </ul>
-        </div>;
-    }, [globalSpellItems]);
+            return (
+                <div>
+                    <p className="bold">{player.name}</p>
+                    <ul className="scene-spell-list">
+                        {playerItems.map((item) => (
+                            <SpellDisplay
+                                key={item.id}
+                                spellID={
+                                    (
+                                        item.metadata[
+                                            spellMetadataKey
+                                        ] as MessageType["spellData"]
+                                    )?.name
+                                }
+                                effectID={
+                                    item.metadata[effectMetadataKey] as string
+                                }
+                                item={item}
+                                caster={player}
+                            />
+                        ))}
+                    </ul>
+                </div>
+            );
+        },
+        [globalSpellItems]
+    );
 
     useEffect(() => {
         if (!obr.ready || !obr.sceneReady || obr.player?.role !== "GM") {
@@ -170,28 +244,32 @@ export default function SceneControls() {
         }
 
         const unmountGlobal = OBR.scene.items.onChange(setGlobalSpellItems);
-        OBR.scene.items.getItems().then(globalItems => {
+        OBR.scene.items.getItems().then((globalItems) => {
             setGlobalSpellItems(globalItems);
         });
 
         return () => {
             unmountGlobal();
-        }
+        };
     }, [obr.ready, obr.sceneReady, setGlobalSpellItems]);
 
-    return <div>
-        {
-            player ? <>
-                <p className="subtitle">Active Effects</p>
-                {
-                    [player, ...party].map(player => (
+    return (
+        <div>
+            {player ? (
+                <>
+                    <Typography variant="h6" className="subtitle">
+                        Active Effects
+                    </Typography>
+                    {[player, ...party].map((player) => (
                         <PlayerEffects key={player.id} player={player} />
-                    ))
-                }
-                {
-                    !spellEffectsPresent && <p>No spell effects in this scene.</p>
-                }
-            </> : <p>No scene selected.</p>
-        }
-    </div>;
+                    ))}
+                    {!spellEffectsPresent && (
+                        <p>No spell effects in this scene.</p>
+                    )}
+                </>
+            ) : (
+                <p>No scene selected.</p>
+            )}
+        </div>
+    );
 }
